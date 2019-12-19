91192c4d2bc191f95c301398f7921dafbe7aecd7
SDK Automation <sdkautomation@microsoft.com>
2019-12-19 10:27:51 +0000

    Generated from d1ee38cd8a24ef63094170616cfeafcf12cfeb47
    run prettier

--- sdk/kusto/azure-mgmt-kusto/azure/mgmt/kusto/_kusto_management_client.py
+++ sdk/kusto/azure-mgmt-kusto/azure/mgmt/kusto/_kusto_management_client.py
@@ -14,9 +14,6 @@ from msrest import Serializer, Deserializer
 
 from ._configuration import KustoManagementClientConfiguration
 from .operations import ClustersOperations
-from .operations import DatabasesOperations
-from .operations import AttachedDatabaseConfigurationsOperations
-from .operations import DataConnectionsOperations
 from .operations import Operations
 from . import models
 
@@ -29,12 +26,6 @@ class KustoManagementClient(SDKClient):
 
     :ivar clusters: Clusters operations
     :vartype clusters: azure.mgmt.kusto.operations.ClustersOperations
-    :ivar databases: Databases operations
-    :vartype databases: azure.mgmt.kusto.operations.DatabasesOperations
-    :ivar attached_database_configurations: AttachedDatabaseConfigurations operations
-    :vartype attached_database_configurations: azure.mgmt.kusto.operations.AttachedDatabaseConfigurationsOperations
-    :ivar data_connections: DataConnections operations
-    :vartype data_connections: azure.mgmt.kusto.operations.DataConnectionsOperations
     :ivar operations: Operations operations
     :vartype operations: azure.mgmt.kusto.operations.Operations
 
@@ -55,17 +46,11 @@ class KustoManagementClient(SDKClient):
         super(KustoManagementClient, self).__init__(self.config.credentials, self.config)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self.api_version = '2019-09-07'
+        self.api_version = '2020-01-01-preview'
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
         self.clusters = ClustersOperations(
-            self._client, self.config, self._serialize, self._deserialize)
-        self.databases = DatabasesOperations(
-            self._client, self.config, self._serialize, self._deserialize)
-        self.attached_database_configurations = AttachedDatabaseConfigurationsOperations(
-            self._client, self.config, self._serialize, self._deserialize)
-        self.data_connections = DataConnectionsOperations(
             self._client, self.config, self._serialize, self._deserialize)
         self.operations = Operations(
             self._client, self.config, self._serialize, self._deserialize)
